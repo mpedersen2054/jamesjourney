0f7fe8c2e363691156ad6d93846f25080b6c6d3f
Matt Pedersen <mpedersen2054@gmail.com>
2015-07-27 01:31:48 -0500

    add code for slidebar offcanvas menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,37 @@
 $(function() {
-  
-  var titles = $('.title, .subtitle');
 
-  titles.widowFix();
+  var controller = new slidebars();
+  controller.init()
+
+  $( '.open-right' ).on( 'click', function ( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+    controller.open( 'sb-1' );
+  });
+
+  $( '.close-right' ).on( 'click', function ( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+    controller.close( 'sb-1' );
+  });
+
+  $( '.close-any' ).on( 'click', function ( event ) {
+    if ( controller.active( 'slidebar' ) ) {
+      event.preventDefault();
+      event.stopPropagation();
+      controller.close();
+    }
+  });
+
+
+
+
+
+  function windowFix() {
+    var titles = $('.title, .subtitle');
+    titles.widowFix();
+  }
+
+  windowFix();
 
 })
