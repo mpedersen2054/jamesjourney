5fb29cac802f1b3e52dd8225fee5096c86efcc0d
Matt Pedersen <mpedersen2054@gmail.com>
2015-07-27 08:25:41 -0500

    add some styles for index jumbotron, add more html and css for slidemenu

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,37 @@
 $(function() {
 
-  var controller = new slidebars();
-  controller.init()
-
-  $( '.open-right' ).on( 'click', function ( event ) {
-    event.preventDefault();
-    event.stopPropagation();
-    controller.open( 'sb-1' );
-  });
-
-  $( '.close-right' ).on( 'click', function ( event ) {
-    event.preventDefault();
-    event.stopPropagation();
-    controller.close( 'sb-1' );
-  });
-
-  $( '.close-any' ).on( 'click', function ( event ) {
-    if ( controller.active( 'slidebar' ) ) {
-      event.preventDefault();
-      event.stopPropagation();
-      controller.close();
-    }
-  });
-
-
-
-
-
   function windowFix() {
     var titles = $('.title, .subtitle');
     titles.widowFix();
   }
 
-  windowFix();
 
-})
+  function overlayMenu() {
+    var controller = new slidebars();
+    controller.init()
+
+    $( '.open-right' ).on( 'click', function ( event ) {
+      event.preventDefault();
+      event.stopPropagation();
+      controller.open( 'sb-1' );
+    });
+
+    $( '.close-right' ).on( 'click', function ( event ) {
+      event.preventDefault();
+      event.stopPropagation();
+      controller.close( 'sb-1' );
+    });
+
+    $( '.close-any' ).on( 'click', function ( event ) {
+      if ( controller.active( 'slidebar' ) ) {
+        event.preventDefault();
+        event.stopPropagation();
+        controller.close();
+      }
+    });
+  }
+
+
+  windowFix();
+  overlayMenu();
+});
