6bc13508ed1e14645055d9a12f8cbcc118964f0d
Matt Pedersen <mpedersen2054@gmail.com>
2015-12-07 14:20:00 -0600

    add some initial styles for recent updates html and css

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -1,2 +1,11 @@
-console.log('hello there')
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoiYXBwLmJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdoZWxsbyB0aGVyZScpIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+
+function power(base, exponent) {
+  if (exponent == 0)
+    return 1;
+  else
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(4, 4));
+// → 8
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuZnVuY3Rpb24gcG93ZXIoYmFzZSwgZXhwb25lbnQpIHtcbiAgaWYgKGV4cG9uZW50ID09IDApXG4gICAgcmV0dXJuIDE7XG4gIGVsc2VcbiAgICByZXR1cm4gYmFzZSAqIHBvd2VyKGJhc2UsIGV4cG9uZW50IC0gMSk7XG59XG5cbmNvbnNvbGUubG9nKHBvd2VyKDQsIDQpKTtcbi8vIOKGkiA4Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello there')
+
+function power(base, exponent) {
+  if (exponent == 0)
+    return 1;
+  else
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(4, 4));
+// → 8
