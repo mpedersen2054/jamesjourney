dab8107f3bcd30f805b8229733af6cd6e329d2c0
Matt Pedersen <mpedersen2054@gmail.com>
2015-12-09 04:44:03 -0600

    fix, try to push to heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 module.exports = function(mongoose) {
-  mongoose.connect('mongodb://localhost/james');
+  mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/james');
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() { console.log('~~ connected to mongodb ~~') });
