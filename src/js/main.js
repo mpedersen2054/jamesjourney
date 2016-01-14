1f5b0cfffee20d7a1cc08f1d541ae707a62c726e
Matt Pedersen <mpedersen2054@gmail.com>
2016-01-14 11:30:04 -0600

    add fancybox to galleries

--- src/js/main.js
+++ src/js/main.js
@@ -290,15 +290,24 @@
   }
 
   App.imageGallery = function() {
+    // once all the images are all loaded init masonry with options
     var $grid = $('#galleries .grid').imagesLoaded(function() {
-
       $grid.masonry({
         itemSelector:    '.grid-item',
         percentPosition: true,
         columnWidth:     '.grid-sizer',
         gutter:          5
       });
+    });
 
+    $('.fancybox').fancybox({
+      fitToView: true,
+      closeBtn: true,
+      padding: '60px 0px 30px 0px',
+      // width: '60%',
+      // height: '60%',
+      maxWidth: 1200,
+      maxHeight: 560
     });
   }
 
