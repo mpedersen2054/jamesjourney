822c6ba6b1a3ccecfc7c66ee76eef874d94a8ddf
Matt Pedersen <mpedersen2054@gmail.com>
2016-01-14 06:11:57 -0600

    adjust css for index page

--- src/js/main.js
+++ src/js/main.js
@@ -189,39 +189,43 @@
       e.preventDefault();
       var $this = $(this);
 
+      // same accross all programs
+      // hide desat img, show sat img
       $this
         .find('.desaturated-img')
           .css({ display: 'none' })
         .end()
         .find('.saturated-img')
           .css({ display: 'block' })
-        .end()
 
+      // if scenario programX
+      // make content width 100%
       if ($this.hasClass('program1')) {
         $this
           .find('.content')
           .css({ width: '100%' })
 
+        // push all over 4%
         $prog2.css({ left: '24%' });
         $prog3.css({ left: '44%' });
         $prog4.css({ left: '64%' });
         $prog5.css({ left: '84%' });
       }
-      else if ($this.hasClass('program2')) {
 
+      else if ($this.hasClass('program2')) {
         $this
           .css({ left: '18%' })
           .find('.content')
           .css({ width: '100%' })
 
+        // left -2% push all to the right 2%
         $prog1.css({ left: '-2%' });
         $prog3.css({ left: '42%' });
         $prog4.css({ left: '62%' });
         $prog5.css({ left: '82%' });
-
       }
-      else if ($this.hasClass('program3')) {
 
+      else if ($this.hasClass('program3')) {
         $this
           .css({ left: '38%' })
           .find('.content')
@@ -231,10 +235,9 @@
         $prog2.css({ left: '18%' });
         $prog4.css({ left: '62%' });
         $prog5.css({ left: '82%' });
-
       }
-      else if ($this.hasClass('program4')) {
 
+      else if ($this.hasClass('program4')) {
         $this
           .css({ left: '58%' })
           .find('.content')
@@ -246,13 +249,14 @@
         $prog5.css({ left: '82%' });
 
       }
-      else if ($this.hasClass('program5')) {
 
+      else if ($this.hasClass('program5')) {
         $this
           .css({ left: '76%' })
           .find('.content')
           .css({ width: '100%' })
 
+        // push all to the left -4%
         $prog1.css({ left: '-4%' });
         $prog2.css({ left: '16%' });
         $prog3.css({ left: '36%' });
@@ -263,6 +267,8 @@
 
     $progAll.on('mouseleave', function(e) {
       e.preventDefault();
+
+      // hide all sat-img, show all desat-img
       $progAll
         .find('.saturated-img')
           .css({ display: 'none' })
@@ -273,6 +279,8 @@
         .find('.content')
         .css({ width: '80%' })
 
+      // return all progams to their
+      // normal state
       $prog1.css({ left: '0%' });
       $prog2.css({ left: '20%' });
       $prog3.css({ left: '40%' });
