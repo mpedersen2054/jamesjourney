7b89cae99205f37e354f53567043e7cdb3343098
Matt Pedersen <mpedersen2054@gmail.com>
2016-01-14 11:48:32 -0600

    add favicon

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(methodOverride('_method'));
 app.use(bodyParser.json());
