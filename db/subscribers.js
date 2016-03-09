96038ae20399dc6d84b2746e1a64264d9fb4bde8
Matt Pedersen <mpedersen2054@gmail.com>
2016-03-09 17:07:55 -0600

    make it so if you subscribe and you arent on the list, it will add you and render the subscribed template, if you are already on the list, it will render the subscribed template with success=false which will show a different block of markup

--- db/subscribers.js
+++ db/subscribers.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
 
 var SubscriberSchema = new mongoose.Schema({
+  mcid: String,
   f_name: String,
   l_name: String,
+  full_name: String,
   email: String,
   added_on: { type: Date, default: Date.now },
-  updated_on: { type: Date }
+  updated_on: { type: Date },
+  events_attending: [mongoose.Schema.Types.ObjectId]
 });
 
 module.exports = mongoose.model('Subscriber', SubscriberSchema);
--- routes/subscribersRouter.js
+++ routes/subscribersRouter.js
@@ -1,16 +1,63 @@
 var subscribersRouter = require('express').Router();
-var mcapi             = require('mailchimp-api');
-var request           = require('request');
-var mc                = new mcapi.Mailchimp('a6eec20e3398ba1010f1243598ee34cb-us11');
+var mailchimpWrapper = require('../lib/mailchimpWrapper');
 
 subscribersRouter.route('/')
   .get(function(req, res) {
-    request('https://us11.api.mailchimp.com/3.0/', {
+    res.send('hello /subscribers root!');
+  })
+
+subscribersRouter.route('/new')
+
+  .get(function(req, res) {
+    res.render('subscribe');
+  })
+
+  .post(function(req, res) {
+    var body = req.body;
+    // use the mailchimpWrapper and pass in the body containing
+    // the email, firstname and lastname, then render subscribed template
+    mailchimpWrapper.addUser(req.body, function(err, resp) {
+      if (err) {
+        res.render('subscribed', {
+          success: false,
+          name: body.f_name+' '+body.l_name,
+          email: body.email
+        })
+      }
+      else {
+        res.render('subscribed', {
+          success: true,
+          name: body.f_name+' '+body.l_name,
+          email: body.email
+        });
+      }
+    });
+  });
+
+
+subscribersRouter.route('/members')
+  .get(function(req, res) {
+    request('https://us11.api.mailchimp.com/3.0/lists/cb90ef9f1e/members', {
       'headers': {
         'Authorization': 'apikey a6eec20e3398ba1010f1243598ee34cb-us11'
       }
     }, function(err, resp, body) {
-      res.send(body)
+      var theResp = JSON.parse(body);
+      var members = theResp.members;
+      var formattedMembers = [];
+
+      for (var i in members) {
+        var member = members[i];
+        var obj = {};
+        obj.id = member.id;
+        obj.emailAddress = member.email_address;
+        obj.fName = decodeURI(member.merge_fields['FNAME']);
+        obj.lName = decodeURI(member.merge_fields['LNAME']);
+        obj.fullName = obj.fName + ' ' + obj.lName;
+
+        formattedMembers.push(obj);
+      }
+      res.json(formattedMembers);
     })
   })
 
