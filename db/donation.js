63822ec667345ec072bfd725968f2fc9cc310984
Matt Pedersen <mpedersen2054@gmail.com>
2016-03-23 06:08:36 -0500

    more code for donation

--- db/donation.js
+++ db/donation.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
 
 var DonationSchema = new mongoose.Schema({
-  f_name: String,
-  l_name: String,
+  uid: { type: mongoose.Schema.Types.ObjectId},
+  type: String,
   full_name: String,
   email: String,
+  refundUrl: String,
+  status: String,
   added_on: { type: Date, default: Date.now },
   updated_on: { type: Date },
-  // events_attending: [mongoose.Schema.Types.ObjectId]
 });
 
 module.exports = mongoose.model('Donation', DonationSchema);
--- routes/donateRouter.js
+++ routes/donateRouter.js
@@ -9,43 +9,77 @@ donateRouter.route('/')
   })
 
   .post(function(req, res) {
-
-    Subscriber.find({ email: req.body.emailAddress }, function(err, sub) {
+    Subscriber.findOne({ email: req.body.emailAddress }, function(err, sub) {
       if (err) { console.log(err); }
 
-      var subscriber;
+      // to pass into stripeWrapper.charges.create
+      var charge = {
+        amount: 2000, // make dynamic
+        currency: 'USD',
+        card: req.body.stripeToken
+      };
 
-      if (!sub.length) {
-        // no sub found
-        var splitName = req.body.nameOnCard.split(' ');
-        var fName = splitName[0], lName = splitName[1];
-        var subObj = {
-          f_name: fName,
-          l_name: lName,
-          email: req.body.emailAddress
+      stripeWrapper.charges.create(charge, function(err, charge) {
+        if (err || !charge) { return console.log('error!', err); }
+
+        // create charge object to save to db
+        var dbCharge = {
+          chid: charge.id,
+          email: req.body.emailAddress,
+          full_name: req.body.nameOnCard,
+          type: charge.object,
+          refundUrl: charge.refunds.url,
+          status: charge.status
         }
-        var newSub = new Subscriber(subObj);
-        newSub.save(function(err) {
-          if (err) { console.log('error!', err); }
-          console.log('new zzubbb')
-        })
-      }
 
-      var zub = sub || newSub;
+        // sub found in db
+        if (sub) {
+          dbCharge.uid = sub._id;
+          var newDonation = new Donation(dbCharge);
+          newDonation.save(function(err, don) {
+            if (err) { console.log('error creating donation', err); }
+            if (!err) {
+              console.log('new donation created!', don);
+              sub.donations.push(don._id);
+              sub.save(function(err) {
+                if (err) { console.log(err) }
+                console.log('saved donation onto sub')
+              });
+            }
+          })
 
-      console.log(zub)
+        // sub not found in db
+        } else {
+          var splitName = req.body.nameOnCard.split(' ');
+          var fName = splitName[0], lName = splitName[1];
+          // get attrs from req.body
+          var subObj = {
+            f_name: fName,
+            l_name: lName,
+            email: req.body.emailAddress
+          }
+          var newSub = new Subscriber(subObj);
+          newSub.save(function(err) {
+            if (err) { console.log('error creating sub', err); }
+            if (!err) {
+              console.log('new sub created!', newSub);
+              dbCharge.uid = newSub._id;
+              var newDonation = new Donation(dbCharge);
+              newDonation.save(function(err) {
+                if (err) { console.log(err); }
+                newSub.donations.push(newDonation._id);
+                newSub.save(function(err) {
+                  if (err) { console.log('error!', err); }
+                  console.log('donation saved onto sub');
+                });
+              })
+            }
+          })
+        }
+
+      })
     })
 
-
-    // var charge = {
-    //   amount: 2000, // make dynamic
-    //   currency: 'USD',
-    //   card: req.body.stripeToken
-    // }
-    // stripeWrapper.charges.create(charge, function(err, charge) {
-    //   if (err) { console.log(err) }
-    //   console.log('successfully charged!', charge);
-    // })
   })
 
 module.exports = donateRouter;
