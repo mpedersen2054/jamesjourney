89d764199647254f347d88413885fd817efa944f
Matt Pedersen <mpedersen2054@gmail.com>
2016-04-27 00:11:20 -0500

    get the masonry gallery to work. need to add a lightbox now

--- src/js/controllers/galleries_controller.js
+++ src/js/controllers/galleries_controller.js
@@ -1,24 +1,42 @@
 
-// import $ from 'jquery';
+import $ from 'jquery';
+import jQueryBridget from 'jquery-bridget';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
+
+jQueryBridget( 'masonry', Masonry, $ );
+imagesLoaded.makeJQueryPlugin( $ );
 
 export function imageGallery() {
-  // once all the images are all loaded init masonry with options
-  var $grid = $('#galleries .grid').imagesLoaded(function() {
+  const $grid = $('#galleries .grid');
+  $grid.imagesLoaded(function() {
     $grid.masonry({
-      itemSelector:    '.grid-item',
+      itemSelector: '.grid-item',
       percentPosition: true,
-      columnWidth:     '.grid-sizer',
-      gutter:          5
-    });
-  });
+      columnWidth: '.grid-sizer'
+    })
+  })
 
-  $('.fancybox').fancybox({
-    fitToView: true,
-    closeBtn:  true,
-    padding:   '60px 0px 30px 0px',
-    // width:  '60%',
-    // height: '60%',
-    maxWidth:  1200,
-    maxHeight: 560
-  });
+
+
 }
+
+// // once all the images are all loaded init masonry with options
+// var $grid = $('#galleries .grid').imagesLoaded(function() {
+//   $grid.masonry({
+//     itemSelector:    '.grid-item',
+//     percentPosition: true,
+//     columnWidth:     '.grid-sizer',
+//     gutter:          5
+//   });
+// });
+
+// $('.fancybox').fancybox({
+//   fitToView: true,
+//   closeBtn:  true,
+//   padding:   '60px 0px 30px 0px',
+//   // width:  '60%',
+//   // height: '60%',
+//   maxWidth:  1200,
+//   maxHeight: 560
+// });
